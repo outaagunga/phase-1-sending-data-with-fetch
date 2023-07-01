54e1ed05611ccd98c981973a9741d197ebabec9a
outaagunga <jeconiaagunga@gmail.com>
2023-07-01 17:26:25 +0300

    Completed the test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+//Write your code here
+
+function submitData(name, email) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({ name, email })
+  };
+
+  return fetch('http://localhost:3000/users', requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += data.id;
+    })
+    .catch(error => {
+      document.body.innerHTML += error.message;
+    });
+}
+
+module.exports = submitData;
